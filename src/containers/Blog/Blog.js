555fd3602d120c5774a02f818f8d06555c195efd
Rohan Borakhatariya <rohan.borakhatariya@CDSYS.LOCAL>
2020-05-29 09:55:58 +0530

    Styling active links using by default class given from NavLink. Writing exact to match only one active link at a time. Removing link fort manifest in index.html because it cause an error in console.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-import { Route, Link } from 'react-router-dom'
+import { Route, NavLink } from "react-router-dom";
 
 import "./Blog.css";
-import Posts from "./Posts/Posts"
-import NewPost from "./NewPost/NewPost"
+import Posts from "./Posts/Posts";
+import NewPost from "./NewPost/NewPost";
 
 class Blog extends Component {
   render() {
@@ -13,20 +13,25 @@ class Blog extends Component {
           <nav>
             <ul>
               <li>
-                <Link to="/">Home</Link>
+                <NavLink to="/" exact>Home</NavLink>
               </li>
               <li>
-                <Link to={{
-                  pathname: "/new-post",
-                  hash: '#submit',
-                  search: '?quick-submit=true'
-                }}>New Post</Link>
+                <NavLink
+                  to={{
+                    pathname: "/new-post",
+                    hash: "#submit",
+                    search: "?quick-submit=true",
+                  }}
+                  exact
+                >
+                  New Post
+                </NavLink>
               </li>
             </ul>
           </nav>
         </header>
-        <Route path="/" exact component={Posts}/>
-        <Route path="/new-post" exact component={NewPost}/>
+        <Route path="/" exact component={Posts} />
+        <Route path="/new-post" exact component={NewPost} />
       </div>
     );
   }
